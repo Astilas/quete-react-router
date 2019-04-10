8a89a2b861dfac303700c3ea9713eb3d37232b63
Thibault Carayon <th.carayon@gmail.com>
2019-04-10 10:07:59 +0200

    creation des composant Home et History et modif du code dans App (lien vers home et history)

---
+++ src/History.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class History extends Component {
+  render() {
+    return (
+      <div>
+         Notre Histoire
+      </div>
+    );
+  }
+}
+
+export default History;
